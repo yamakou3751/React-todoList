fe5cdd579877eea964b1d52833fcf02bad225acc
kouhei yamanaka <home3751@gmail.com>
2021-03-04 14:08:19 +0900

    initial commit

---
+++ src/components/CompleteTodos.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const CompleteTodos = (props) => {
+  const { todos, onClick } = props;
+  return (
+    <div className="complete-area">
+      <p className="title">完了のTODO</p>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <div key={index} className="list-row">
+              <li>{todo}</li>
+              <button onClick={() => onClick(index)}>戻る</button>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
